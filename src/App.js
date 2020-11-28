a738d524ca6e7781c601f4eb3dee06eb31fc8ad6
Bruno <brunochirelli@gmail.com>
2020-11-28 17:25:55 -0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          {/* <Route exact path="/carrinho" component={Cart} /> */}
+          {/* <Route exact path="/produtos" component={Gifts} /> */}
+          <Route exact path="/" render={() => <div>Home</div>} />
+          <Route render={() => <div>404</div>} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
