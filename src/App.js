3c761750f75d60d2236f6761376389f6c12106d7
Bruno <brunochirelli@gmail.com>
2020-12-01 11:22:11 -0300

    chore gifts rout name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Cart from "./components/cart/Cart";
 import Gifts from "./components/gifts/Gifts";
+import GiftSingle from "./components/gifts/GiftSingle";
 import Layout from "./components/layout/Layout";
 import NotFound from "./components/layout/NotFound";
 
@@ -10,7 +11,8 @@ const App = () => {
       <Layout>
         <Switch>
           <Route exact path="/carrinho" component={Cart} />
-          <Route exact path="/produtos" component={Gifts} />
+          <Route exact path="/presentes" component={Gifts} />
+          <Route exact path="/presentes/:gift" component={GiftSingle} />
           <Route exact path="/" render={() => <div>Home</div>} />
           <Route component={NotFound} />
         </Switch>
