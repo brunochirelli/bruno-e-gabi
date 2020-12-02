386ced5929797596c7ca43322cfbd00e1356e8a7
Bruno <brunochirelli@gmail.com>
2020-12-02 08:52:06 -0300

    fixes header position

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   AppBar,
   Badge,
@@ -9,16 +9,19 @@ import {
 import { Menu, ShoppingCart } from "@material-ui/icons";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import CartDrawer from "../cart/CartDrawer";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   const cartGifts = useSelector((state) => state.cart.products);
 
   return (
-    <AppBar position="static" elevation={0} color="inherit">
+    <AppBar position="fixed" elevation={0} color="inherit" id="main-header">
       <Toolbar style={{ justifyContent: "space-between" }}>
         <IconButton>
           <Menu />
         </IconButton>
+
         <Typography
           variant="h6"
           component={Link}
@@ -27,11 +30,12 @@ const Header = () => {
         >
           Bruno e Gabi
         </Typography>
-        <IconButton component={Link} to="/carrinho">
+        <IconButton onClick={() => setOpen(!open)}>
           <Badge badgeContent={cartGifts && cartGifts.length} color="primary">
             <ShoppingCart />
           </Badge>
         </IconButton>
+        <CartDrawer open={open} setOpen={setOpen} />
       </Toolbar>
     </AppBar>
   );
