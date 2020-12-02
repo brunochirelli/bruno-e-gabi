6f814410bc6edf2032defda3755fd9542a454c02
Bruno <brunochirelli@gmail.com>
2020-12-02 08:51:46 -0300

    set dynamic header

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
+  const [headerHeight, setHeaderHeight] = useState(56);
+
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      setHeaderHeight(document.getElementById("main-header").offsetHeight);
+  }, []);
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main style={{ marginTop: headerHeight + "px" }}>{children}</main>
     </>
   );
 };
