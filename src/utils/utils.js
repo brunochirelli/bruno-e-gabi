25c9544740a0a8b075d2901152e73d87fe77183d
Bruno <brunochirelli@gmail.com>
2020-12-04 09:09:53 -0300

    remove util index

--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const truncateText = (str, maxLength) => {
-  if (typeof str === "string" && typeof maxLength === "number")
-    return str.split("").length > maxLength
-      ? `${str.substring(0, maxLength - 3)}...`
-      : str;
-  else return str;
-};
