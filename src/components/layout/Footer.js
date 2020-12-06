ee9395f5b11337b8ded97bd80c29e064165a0bde
Bruno <brunochirelli@gmail.com>
2020-12-06 20:46:32 -0300

    initial component boilerplate

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return <FancyFooter>asdasdasd</FancyFooter>;
+};
+
+const FancyFooter = styled.footer`
+  background: #faf3ec;
+`;
+
+export default Footer;
