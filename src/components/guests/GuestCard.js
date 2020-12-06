c7db9248e1873799830b2c815a6030f131f004d4
Bruno <brunochirelli@gmail.com>
2020-12-06 08:42:22 -0300

    add guest card

--- src/components/guests/GuestCard.js
+++ src/components/guests/GuestCard.js
@@ -1,21 +1,55 @@
+import PropTypes from "prop-types";
 import { Box, Paper, Typography } from "@material-ui/core";
+import styled from "styled-components";
+
+/**
+ * Cart Page
+ *
+ * @component
+ * @version       0.1.0
+ * @description   Map all Cart Items and proceed checkout.
+ *
+ */
 
 const GuestCard = ({ guest, updateFunction }) => {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      padding="1rem"
-      margin="0.5rem auto"
-      style={{ cursor: "pointer", opacity: guest.confirmed ? 0.5 : 1 }}
+    <FancyGuestCard
       component={Paper}
       onClick={() => updateFunction(guest, "confirmed", !guest.confirmed)}
+      style={{ opacity: guest.confirmed ? 0.5 : 1 }}
     >
-      <Typography noWrap>{guest.name}</Typography>
-      <Typography>{guest.confirmed ? "Confirmado" : "Confirmar"}</Typography>
-    </Box>
+      <Box>
+        <Typography variant="h6" noWrap>
+          {guest.name}
+        </Typography>
+        <Typography variant="caption">
+          {guest.confirmed ? "Confirmado" : null}
+        </Typography>
+      </Box>
+      <Typography>{guest.confirmed ? "Desconfirmar" : "Confirmar"}</Typography>
+    </FancyGuestCard>
   );
+};
+
+const FancyGuestCard = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0.5rem auto;
+  padding: 1rem;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: none;
+  }
+`;
+
+GuestCard.propTypes = {
+  guest: PropTypes.shape({
+    name: PropTypes.string,
+    confirmed: PropTypes.bool.isRequired,
+  }).isRequired,
+  updateFunction: PropTypes.func.isRequired,
 };
 
 export default GuestCard;
